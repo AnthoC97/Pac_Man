655f4e3c3c5061f936e63aa5b0a64db8ae08bfeb
Bastien PERROTEAU <bastienperoteau@gmail.com>
2018-10-16 12:39:08 +0200

    Merge extended

--- Assets/Scripts/ButtonScript.cs
+++ Assets/Scripts/ButtonScript.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class ButtonScript : MonoBehaviour {
-
+	
 	public void ButtonQuit()
 	{
 		Application.Quit();
--- Assets/Scripts/GameManagerScript.cs
+++ Assets/Scripts/GameManagerScript.cs
@@ -1,20 +1,83 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class GameManagerScript : MonoBehaviour
 {
 
-	[Header("Players/IAs")]
+	[Header("Player/IA One")]
 	[SerializeField] private GameObject PlayerOne;
+	[SerializeField] private GameObject WinOne;
+	[SerializeField] private GameObject LoseOne;
+	private Vector3 P1Init;
 
+	[Header("Player/IA Two")]
 	[SerializeField] private GameObject PlayerTwo;
+	[SerializeField] private GameObject WinTwo;
+	[SerializeField] private GameObject LoseTwo;
+	private Vector3 P2Init;
+
+	[SerializeField] private GameObject EndMenu;
+
+	public bool InGame = false;
 	
-	void Start () {
-		
+	public void ButtonQuit()
+	{
+		Application.Quit();
+	}
+
+	public void ButtonPlayMenuSelection()
+	{
+		SceneManager.LoadScene("SelectionMenu");
+	}
+
+	public void ButtonMainMenu()
+	{
+		SceneManager.LoadScene("MainMenu");
+	}
+
+	public void ButtonReplay()
+	{
+		PlayerOne.transform.position = P1Init;
+		PlayerTwo.transform.position = P1Init;
+		// A voir Inititaliser position gum en random
+		// bool les joueur à false
+		// set position joueur
+	}
+	void Start ()
+	{
+		P1Init = PlayerOne.transform.position;
+		P2Init = PlayerTwo.transform.position;
 	}
 	
 	void Update () {
-		
+		if (InGame)
+		{
+			if (PlayerOne.transform.position.x < PlayerTwo.transform.position.x + PlayerTwo.transform.localScale.x
+			    && PlayerOne.transform.position.x > PlayerTwo.transform.position.x - PlayerTwo.transform.localScale.x)
+			{
+				if (PlayerOne.transform.position.z < PlayerTwo.transform.position.z + PlayerTwo.transform.localScale.z
+				    && PlayerOne.transform.position.z > PlayerTwo.transform.position.z - PlayerTwo.transform.localScale.z)
+				{
+					/*
+					if (PlayerOne.GetComponent<IntentScript>().IsKiller)
+					{
+						InGame = false;
+						WinOne.SetActive(true);
+						LoseTwo.SetActive(true);
+						EndMenu.SetActive(true);
+					}
+					else if(PlayerTwo.GetComponent<IntentScript>().IsKiller)
+					{
+						InGame = false;
+						WinTwo.SetActive(true);
+						LoseOne.SetActive(true);
+						EndMenu.SetActive(true);
+					}
+					*/
+				}
+			}
+		}
 	}
 }
