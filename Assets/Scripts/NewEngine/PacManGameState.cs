0170e391b7d0aa90f76b7a5502a5cf533b483a80
Bastien PERROTEAU <bastienperoteau@gmail.com>
2018-10-17 17:13:36 +0200

    PacMan GameState Update

--- Assets/Scripts/NewEngine/PacManGameState.cs
+++ Assets/Scripts/NewEngine/PacManGameState.cs
@@ -12,9 +12,13 @@ public enum MovementIntent
 
 public class PacManGameState{
     private int[,] EtatCase;
+    private Vector3 P1, P2, GumBall;
+    private bool P1Killer, P2Killer, GumActive = false;
 
-    public PacManGameState(int x, int z)
+    //Constructeur
+    public PacManGameState(int x, int z, Vector3 p1, Vector3 p2, Vector3 gumball, bool p1k, bool p2k, bool gumact, List<Transform> ListObstacle, List<Transform> ListDoors)
     {
+        // Initialisation à 0 
         for (int i = 0; i < x; i++)
         {
             for (int j = 0; j < z; j++)
@@ -22,15 +26,32 @@ public class PacManGameState{
                 EtatCase[i, j] = 0;
             }
         }
+        // Statut des obstacles mis à 1
+        foreach (Transform cpt in ListObstacle)
+        {
+            EtatCase[(int)cpt.position.x,(int)cpt.position.z] = 1;
+        }
+        // Statut des Portails mis à 2
+        foreach (Transform cpt in ListDoors)
+        {
+            EtatCase[(int)cpt.position.x,(int)cpt.position.z] = 2;
+        }
+        P1 = p1;
+        P2 = p2;
+        GumBall = gumball;
+        P1Killer = p1k;
+        P2Killer = p2k;
+        GumActive = gumact;
     }
-
-    public static int Step(PacManGameState p, int action1, int action2)
-    {
-        return 0;
-    }
-
+    // Copie du GameState
     public PacManGameState Copy()
     {
         return this;
+    }
+
+    // Récupère Intent et Applique changement de position
+    public static int Step(PacManGameState p, MovementIntent action1, MovementIntent action2)
+    {
+        return 0;
     }
 }
