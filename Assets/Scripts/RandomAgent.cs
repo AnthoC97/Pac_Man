f6253559f8f6a83b6ea9906b97a670b8df881a1c
Florian CHAMPAUD <fchampaud@kyriopolis.fr>
2018-10-17 20:08:33 +0200

    Adapt agent scripts to IAgent interface, fix calls

--- Assets/Scripts/RandomAgent.cs
+++ Assets/Scripts/RandomAgent.cs
@@ -2,13 +2,13 @@ using System;
 
 public class RandomAgent : IAgent
 {
-    public override MovementAction act(PacManGameState gs, int playerNumber) {
+    public MovementIntent Act(PacManGameState gs, int playerNumber) {
         var movementActionValues = (MovementAction[]) Enum.GetValues(typeof(MovementAction));
         var actionIndex = UnityEngine.Random.Range(0, movementActionValues.Length);
-        return (MovementAction) movementActionValues.GetValue(actionIndex);
+        return (MovementIntent) movementActionValues.GetValue(actionIndex);
     }
 
-    public override void obs(float reward, bool terminal) {
+    public void Obs(float reward, bool terminal) {
         return;
     }
 }
