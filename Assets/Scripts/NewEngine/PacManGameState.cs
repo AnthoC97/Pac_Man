cf7d96040fccffbebd6882c8485af80713cc09c6
Bastien PERROTEAU <bastienperoteau@gmail.com>
2018-10-18 15:14:05 +0200

    PortalFix

--- Assets/Scripts/NewEngine/PacManGameState.cs
+++ Assets/Scripts/NewEngine/PacManGameState.cs
@@ -9,11 +9,12 @@ public class PacManGameState{
     private int[,] EtatCase;
     private Vector3 P1, P2, GumBall;
     private bool P1Killer, P2Killer, GumActive = false;
+    private int XSize, ZSize;
 
     //Constructeur
-    public PacManGameState(int x, int z, Vector3 p1, Vector3 p2, List<Transform> ListObstacle, List<Transform> ListDoors) {
-        EtatCase = new int[x, z];
-        // Initialisation à 0
+    public PacManGameState(int x, int z, Vector3 p1, Vector3 p2, List<Transform> ListObstacle, List<Transform> ListDoors)
+    {
+        // Initialisation à 0 
         for (int i = 0; i < x; i++)
         {
             for (int j = 0; j < z; j++)
@@ -31,6 +32,9 @@ public class PacManGameState{
         {
             EtatCase[(int)cpt.position.x,(int)cpt.position.z] = 2;
         }
+
+        XSize = x;
+        ZSize = z;
         P1 = p1;
         P2 = p2;
         P1Killer = false;
@@ -100,16 +104,16 @@ public class PacManGameState{
         }
         return new bool[3];
     }
-
+    
     /**
 	 * Returns whether passed GameObject collides with a wall
 	 */
     private static bool CollidesWithWalls(Vector3 player, PacManGameState p)
     {
-        float borderLeft = player.x - 0.35f / 2;
-        float borderRight = player.x + 0.35f / 2;
-        float borderUp = player.z - 0.35f / 2;
-        float borderDown = player.z + 0.35f / 2;
+        float borderLeft = player.x - 0.375f / 2;
+        float borderRight = player.x + 0.375f / 2;
+        float borderUp = player.z - 0.375f / 2;
+        float borderDown = player.z + 0.375f / 2;
 
         // Works because we are axis aligned and player is not wider than walls
         return p.EtatCase[(int) (borderLeft + .5), (int) (borderUp + .5)] == 1 ||
@@ -117,7 +121,7 @@ public class PacManGameState{
                p.EtatCase[(int) (borderRight + .5), (int) (borderUp + .5)] == 1 ||
                p.EtatCase[(int) (borderRight + .5), (int) (borderDown + .5)] == 1;
     }
-
+    
     /**
 	 * Tries to move a player in a direction, cancelling movement on collision
 	 */
@@ -128,7 +132,7 @@ public class PacManGameState{
             player = prevPosition;
         }
     }
-
+    
     // Setter mouvement
     private static void IntentManagement(MovementIntent Intent, Vector3 Player, float Speed, PacManGameState p) {
         if ((Intent & MovementIntent.WantToMoveForward) != 0) {
@@ -145,16 +149,38 @@ public class PacManGameState{
         if ((Intent & MovementIntent.WantToMoveRight) != 0) {
             tryMovingInDirection(Player, Vector3.right, Speed, p);
         }
+        Portal(p.P1,0,p.XSize-1,0,p.ZSize-1);
+        Portal(p.P2,0,p.XSize-1,0,p.ZSize-1);
+    }
+    // Passage de Portail
+    private static void Portal(Vector3 Player, int NorthX, int SouthX, int EastZ, int WestZ)
+    {
+        if (WestZ <= Player.z + (0.375f / 2))
+        {
+            Player = new Vector3(Player.x,Player.y,EastZ + (0.375f));
+        }
+        else if (EastZ >= Player.z - (0.375f / 2))
+        {
+            Player = new Vector3(Player.x,Player.y,WestZ - (0.375f));
+        }
+        else if (SouthX <= Player.x + (0.375f / 2))
+        {
+            Player= new Vector3(NorthX + (0.375f),Player.y,Player.z);
+        }
+        else if (NorthX >= Player.x - (0.375f / 2))
+        {
+            Player = new Vector3(SouthX - (0.375f),Player.y,Player.z);
+        }
     }
     // Random Gumball Position
     private Vector3 RandGumball()
     {
-        int X = UnityEngine.Random.Range(0, /*EtatCase.Length*/ 17); // TODO FIX
-        int Z = UnityEngine.Random.Range(0, /*EtatCase.Length*/ 17);
+        int X = UnityEngine.Random.Range(0, XSize);
+        int Z = UnityEngine.Random.Range(0, ZSize);
         while (EtatCase[X,Z] != 0)
         {
-            X = UnityEngine.Random.Range(0, /*EtatCase.Length*/ 17);
-            Z = UnityEngine.Random.Range(0, /*EtatCase.Length*/ 17);
+            X = UnityEngine.Random.Range(0, XSize);
+            Z = UnityEngine.Random.Range(0, ZSize);
         }
         return new Vector3(X,0.3f,Z);
     }
