104b385526b31a578306f488638f3fae6a425227
Bastien PERROTEAU <bastienperoteau@gmail.com>
2018-10-19 11:36:35 +0200

    Hierachy Cleaned + Random fix

--- Assets/Scripts/Agent/RandomAgent.cs
+++ Assets/Scripts/Agent/RandomAgent.cs
@@ -6,7 +6,7 @@ using System;
 public class RandomAgent : IAgent
 {
     public MovementIntent Act(PacManGameState gs, int playerNumber) {
-        var movementActionValues = (MovementAction[]) Enum.GetValues(typeof(MovementAction));
+        var movementActionValues = (MovementIntent[]) Enum.GetValues(typeof(MovementIntent));
         var actionIndex = UnityEngine.Random.Range(0, movementActionValues.Length);
         return (MovementIntent) movementActionValues.GetValue(actionIndex);
     }
